2be5b6e26b802c26f3602fb468230c2cf42df198
김정현 <kimjunghyun@gimjeonghyeon-ui-MacBookAir.local>
2025-02-28 15:23:30 +0900

    20250228 - 카카오앱로그인연결 및 파일 삭제 반영

---
+++ src/screens/EmotionDiary.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Counsel = () => {
+
+    return(
+        <View>
+            <Text>감정 일기 스크린</Text>
+        </View>
+    );
+};
+ 
+
+export default Counsel;
